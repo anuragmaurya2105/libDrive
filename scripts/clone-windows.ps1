995ce7ac64999cf5a455566e4d58c8310e79222f
Anupam Maurya <83022538+anuragmaurya2105@users.noreply.github.com>
2022-07-08 22:04:44 +0530

    Update clone-windows.ps1

--- scripts/clone-windows.ps1
+++ scripts/clone-windows.ps1
@@ -1,11 +1,11 @@
 cd ..
 
-git clone https://github.com/libDrive/cloudflare.git
-git clone https://github.com/libDrive/config.git
-git clone https://github.com/libDrive/heroku.git
-git clone https://github.com/libDrive/libdrive.github.io.git
-git clone https://github.com/libDrive/server.git
-git clone https://github.com/libDrive/web.git
-git clone https://github.com/libDrive/webhooks.git
+git clone https://github.com/anuragmaurya2105/cloudflare.git
+git clone https://github.com/anuragmaurya2105/config.git
+git clone https://github.com/anuragmaurya2105/heroku.git
+git clone https://github.com/anuragmaurya2105/libdrive.github.io.git
+git clone https://github.com/anuragmaurya2105/server.git
+git clone https://github.com/anuragmaurya2105/web.git
+git clone https://github.com/anuragmaurya2105/webhooks.git
 
 cd libDrive
